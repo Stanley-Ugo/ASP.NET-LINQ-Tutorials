e9b7cb01526f4a5e33b9ce4b4e974c265d5ea531
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-01-07 07:33:25 +0100

    Returning the input string

--- ExtensionMethodsDemo/StringHelper.cs
+++ ExtensionMethodsDemo/StringHelper.cs
@@ -15,7 +15,11 @@ namespace ExtensionMethodsDemo
             {
                 char[] charArray = inputString.ToCharArray();
                 charArray[0] = char.IsUpper(charArray[0]) ? char.ToLower(charArray[0]) : char.ToUpper(charArray[0]);
+
+                return new string(charArray);
             }
+
+            return inputString;
         }
     }
 }
