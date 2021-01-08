ec668d4cc03ca50e94de2825b5a34260bff081ec
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-01-08 06:44:19 +0100

    Getting the Min & Max Values with LINQ

---
+++ ExtensionMethodsDemo/AggregateFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsDemo
+{
+    public class AggregateFunctions
+    {
+        public int AggregateMinAndMax()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //Getting the Min even numbers Values with LINQ
+            int result = numbers.Where(x => x % 2 == 0).Min();
+
+            //Getting the Max even numbers Values with LINQ
+            int resultII = numbers.Where(x => x % 2 == 0).Max();
+
+            return result;
+        }
+    }
+}
