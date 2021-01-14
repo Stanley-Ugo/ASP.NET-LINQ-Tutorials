98d94cba74d59d502e646dd8c979d4ff6cd6214c
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-01-15 00:09:52 +0100

    Fixing indentation

--- ExtensionMethodsDemo/AggregateFunctions.cs
+++ ExtensionMethodsDemo/AggregateFunctions.cs
@@ -271,8 +271,6 @@ namespace ExtensionMethodsDemo
             //chects to see if an element is present in a sequence
             var result4 = numbers.Contains(3);
 
-
-
             Console.WriteLine(result);
         }
     }
