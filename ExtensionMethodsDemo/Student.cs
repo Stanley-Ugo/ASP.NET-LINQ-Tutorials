9565113d8b017f1efb094a5337502d3881ff6684
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-01-17 01:10:31 +0100

    Adding a student Name

--- ExtensionMethodsDemo/Student.cs
+++ ExtensionMethodsDemo/Student.cs
@@ -121,6 +121,8 @@ namespace ExtensionMethodsDemo
                 new Student
                 {
                     StudentId = 112,
+                    Name = "Henry",
+                    Gender = "Male"
                 }
             };
 
