10a114dfd11f8b6b1dfe4d5fae07e750c46a7b6e
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-01-20 00:07:43 +0100

    Adding a new Employee Last Name

--- ExtensionMethodsDemo/Employee.cs
+++ ExtensionMethodsDemo/Employee.cs
@@ -69,6 +69,7 @@ namespace ExtensionMethodsDemo
                 {
                     EmployeeId = 106,
                     FirstName = "Andrew",
+                    LastName = "Salt"
                 }
             };
 
