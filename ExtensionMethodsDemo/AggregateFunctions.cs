16187a62d616e1219036dd315efc76e303e199b8
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-02-03 06:51:09 +0100

    Using the .Any Linq Extension Method

--- ExtensionMethodsDemo/AggregateFunctions.cs
+++ ExtensionMethodsDemo/AggregateFunctions.cs
@@ -272,10 +272,10 @@ namespace ExtensionMethodsDemo
         {
             int[] numbers = { 1, 2, 3, 4, 5 };
 
-            //result returns true cos all numbers is less than 10 
+            //result returns true cos all numbers is less than 10
             var result = numbers.All(x => x < 10);
 
-            //returns true cos it contains any element
+            //returns true cos it contains any element 
             var result2 = numbers.Any();
 
             //returns false cos no number in the numbers array is greater then 10
