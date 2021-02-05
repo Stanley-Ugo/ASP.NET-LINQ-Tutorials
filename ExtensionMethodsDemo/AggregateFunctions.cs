baf26fc2921c5189cd1b427a32a23164b5139298
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-02-05 07:02:12 +0100

    Sorting OPerators In Linq

--- ExtensionMethodsDemo/AggregateFunctions.cs
+++ ExtensionMethodsDemo/AggregateFunctions.cs
@@ -136,7 +136,7 @@ namespace ExtensionMethodsDemo
         }
 
 
-        //Sorting OPerators 
+        //Sorting OPerators
         public void SortingOperators()
         {
             IOrderedEnumerable<Student> result = Student.GetAllStudents().OrderBy(s => s.TotalMarks).ThenBy(s => s.Name).ThenByDescending(s => s.StudentId);
