75da00b77d32f9904678aaea553a8d8987e30d31
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-02-21 08:20:59 +0100

    Adding New Student Subject

--- ExtensionMethodsDemo/Student.cs
+++ ExtensionMethodsDemo/Student.cs
@@ -134,7 +134,7 @@ namespace ExtensionMethodsDemo
                     StudentId = 113,
                     Name = "Sharon Stone",
                     Gender = "Female",
-                    Subjects = new List<string> {"ASP.NET CORE", "LINQ"},
+                    Subjects = new List<string> {"ASP.NET CORE", "LINQ"}
                     Class = "Entry Level",
                     TotalMarks = 400
                 }
