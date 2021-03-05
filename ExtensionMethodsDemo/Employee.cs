556848b214a6a138509693a4cdfca33d9f179eeb
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-03-05 04:51:08 +0100

    Adding New Employees Last Name

--- ExtensionMethodsDemo/Employee.cs
+++ ExtensionMethodsDemo/Employee.cs
@@ -78,7 +78,7 @@ namespace ExtensionMethodsDemo
                 {
                     EmployeeId = 108,
                     FirstName = "Minn Shin",
-                    LastName = "Monroe",
+                    LastName = "Monroe"
                     Gender = "Male",
                     AnnualSalary = 4000
                 }
